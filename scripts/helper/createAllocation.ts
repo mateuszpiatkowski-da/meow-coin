0eac0a99876c49e2e7bc5d55ab3dbbaf24f6607e
Mateusz Piątkowski <mateusz.piatkowski@digitalasset.com>
2026-02-09 17:58:33 +0100

    feat: implement createAllocation helper

--- scripts/helper/createAllocation.ts
+++ scripts/helper/createAllocation.ts
@@ -4,71 +4,42 @@ import sdk from 'src/util/walletSDK';
 import { type WrappedCommand } from '@canton-network/wallet-sdk';
 import admin from 'src/util/admin';
 import { type components as ledgerClientComponents } from '@canton-network/core-ledger-client';
-import allocationService from 'src/api/allocation-instruction/service';
 import helperInitializer from './util/helperInitializer';
 import { randomUUIDv7 } from 'bun';
 import { createLogger } from 'src/util/logger';
-import {
-  AllocationFactory,
-  AllocationFactory_Allocate,
-} from '@daml-ts/splice-api-token-1.0.0/lib/Splice/Api/Token/AllocationInstructionV1';
-import emptyMeta from './util/emptyMeta';
 import emptyExtraArgs from './util/emptyExtraArgs';
-import { CoinAllocationInstruction } from '@daml-ts/test-coin-1.0.0/lib/Coin/Allocation/Instruction/module';
-import { A } from 'splice-wallet-kernel/examples/portfolio/dist/assets/index-BnJGUN3w';
+import createAllocationInstruction from './createAllocationInstruction';
+import {
+  AllocationInstruction,
+  AllocationInstruction_Update,
+} from '@daml-ts/splice-api-token-1.0.0/lib/Splice/Api/Token/AllocationInstructionV1/module';
+import { CoinAllocation } from '@daml-ts/test-coin-1.0.0/lib/Coin/Allocation/Allocation/module';
 
 const logger = createLogger({ name: 'createTransferInstruction' });
 
 const createAllocation = async () => {
   await helperInitializer.init();
 
-  const { sender, receiver, tokenCid } = helperInitializer.data;
+  const { sender } = helperInitializer.data;
 
-  await allocationService.addSenders([sender.partyId]);
-  const factoryID = await allocationService.getFactoryCID();
+  const allocationInstructionCid = await createAllocationInstruction();
 
-  const allocationFactoryAllocateCommand: WrappedCommand<'ExerciseCommand'> = {
+  const allocationCreateCommand: WrappedCommand<'ExerciseCommand'> = {
     ExerciseCommand: {
-      templateId: AllocationFactory.templateId,
-      contractId: factoryID,
-      choice: 'AllocationFactory_Allocate',
+      templateId: AllocationInstruction.templateId,
+      contractId: allocationInstructionCid,
+      choice: 'AllocationInstruction_Update',
       choiceArgument: {
-        expectedAdmin: admin.partyId,
-        allocation: {
-          settlement: {
-            executor: admin.partyId, // no point in creating another party,
-            settlementRef: {
-              id: randomUUIDv7(),
-              cid: null,
-            },
-            requestedAt: helperInitializer.requestedAt.toISOString(),
-            allocateBefore: helperInitializer.executeBefore.toISOString(),
-            settleBefore: helperInitializer.executeBefore.toISOString(), // no point in creating another deadline (even though time should be strictly after `allocateBefore`)
-            meta: emptyMeta,
-          },
-          transferLegId: `${sender.partyId}__${receiver.partyId}`,
-          transferLeg: {
-            sender: sender.partyId,
-            receiver: receiver.partyId,
-            amount: '100.0',
-            instrumentId: {
-              admin: admin.partyId,
-              id: randomUUIDv7(),
-            },
-            meta: emptyMeta,
-          },
-        },
-        requestedAt: helperInitializer.requestedAt.toISOString(),
-        inputHoldingCids: [tokenCid],
+        extraActors: [sender.partyId, admin.partyId], // passing admin as the executor party
         extraArgs: emptyExtraArgs,
-      } satisfies AllocationFactory_Allocate,
+      } satisfies AllocationInstruction_Update,
     },
   };
 
   await sdk.userLedger?.setPartyId(sender.partyId);
 
   const signCompletionResult = await sdk.userLedger?.prepareSignExecuteAndWaitFor(
-    allocationFactoryAllocateCommand,
+    allocationCreateCommand,
     [
       {
         partyId: admin.partyId,
@@ -84,14 +55,13 @@ const createAllocation = async () => {
 
   const result = await sdk.userLedger?.getEventsByUpdateId(signCompletionResult!.updateId);
 
-  const allocationInstructionEvent = result?.find(
-    (event) =>
-      'CreatedEvent' in event && event.CreatedEvent.templateId === CoinAllocationInstruction.templateIdWithPackageId,
+  const allocationEvent = result?.find(
+    (event) => 'CreatedEvent' in event && event.CreatedEvent.templateId === CoinAllocation.templateIdWithPackageId,
   ) as { CreatedEvent: ledgerClientComponents['schemas']['CreatedEvent'] };
 
-  logger.info({ allocationInstructionCid: allocationInstructionEvent.CreatedEvent.contractId });
+  logger.info({ allocationCid: allocationEvent.CreatedEvent.contractId });
 
-  return '';
+  return allocationEvent.CreatedEvent.contractId;
 };
 
 export default createAllocation;
